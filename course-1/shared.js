167d75c15b59b404ac2bbcab3bf1271812033014
kimjinsoo <roorooro9933@gmail.com>
2020-12-08 16:05:57 +0900

    manipulating modal with class

--- course-1/shared.js
+++ course-1/shared.js
@@ -1,12 +1,37 @@
 
 const backdrop = document.querySelector('.backdrop')
 const modal = document.querySelector('.modal')
+const modalNoButton = document.querySelector('.modal__action--negative')
 const selectPlanButtons = document.querySelectorAll('.plan button')
+const toggleButton = document.querySelector('.toggle-button')
+const mobileNav = document.querySelector('.mobile-nav')
 
 for(let i = 0 ; i < selectPlanButtons.length ; i++){
     selectPlanButtons[i].addEventListener('click',function(){
-        modal.style.display='block'
-        backdrop.style.display='block'
+        // modal.style.display='block'
+        // backdrop.style.display='block'
+        // modal.className = 'open' This will actually overwrite the complete className
+        modal.classList.add('open')
+        backdrop.classList.add('open')
+
     })
 }
 
+backdrop.addEventListener('click',function(){
+    closeModal()
+})
+
+modalNoButton.addEventListener('click',closeModal)
+
+function closeModal(){
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    mobileNav.classList.remove('open')
+    modal.classList.remove('open')
+    backdrop.classList.remove('open')
+}
+
+toggleButton.addEventListener('click',function(){
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
+})
